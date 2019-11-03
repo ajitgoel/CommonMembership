9440e1ae03873402ebeb3d7bff37a9ee492a3d3c
Ajit Goel <ajitgoel@gmail.com>
2019-11-03 07:28:19 -0600

    integrate vuetify

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ if (Meteor.isClient)
   import './imports/client';
   //import './imports/client/logging';  
   import 'vuetify/dist/vuetify.min.css';
+  import 'material-design-icons-iconfont/dist/material-design-icons.css'; 
 } 
 else if (Meteor.isServer) 
 {
