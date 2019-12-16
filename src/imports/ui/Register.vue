0574e228fa183791c8dae0911352282d290dd4ea
Ajit Goel <ajitgoel@gmail.com>
2019-12-15 22:33:59 -0600

    Register user spec

--- src/imports/ui/Register.vue
+++ src/imports/ui/Register.vue
@@ -25,7 +25,7 @@
                     <div class="input-group-prepend">
                       <span class="input-group-text"><i class="fas fa-user"></i></span>
                     </div>
-                    <input type="email" class="form-control" placeholder="name@example.com" autocomplete="off" 
+                    <input type="email" data-cy="email" class="form-control" placeholder="name@example.com" autocomplete="off" 
                     v-focus v-model="user.email" id="email" name="email" 
                     :class="{ 'is-invalid': submitted && ($v.user.email.$error || this.user.userExistsforDomain) }"                                
                     style="background-image: url(&quot;data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAABHklEQVQ4EaVTO26DQBD1ohQWaS2lg9JybZ+AK7hNwx2oIoVf4UPQ0Lj1FdKktevIpel8AKNUkDcWMxpgSaIEaTVv3sx7uztiTdu2s/98DywOw3Dued4Who/M2aIx5lZV1aEsy0+qiwHELyi+Ytl0PQ69SxAxkWIA4RMRTdNsKE59juMcuZd6xIAFeZ6fGCdJ8kY4y7KAuTRNGd7jyEBXsdOPE3a0QGPsniOnnYMO67LgSQN9T41F2QGrQRRFCwyzoIF2qyBuKKbcOgPXdVeY9rMWgNsjf9ccYesJhk3f5dYT1HX9gR0LLQR30TnjkUEcx2uIuS4RnI+aj6sJR0AM8AaumPaM/rRehyWhXqbFAA9kh3/8/NvHxAYGAsZ/il8IalkCLBfNVAAAAABJRU5ErkJggg==&quot;); background-repeat: no-repeat; background-attachment: scroll; background-size: 16px 18px; background-position: 98% 50%; cursor: auto;">                      
@@ -48,7 +48,7 @@
                     <div class="input-group-prepend">
                       <span class="input-group-text"><i class="fas fa-key"></i></span>
                     </div>
-                    <input type="password" class="form-control" placeholder="********" 
+                    <input type="password" data-cy="password" class="form-control" placeholder="********" 
                     v-model="user.password" id="password" name="password" :class="{ 'is-invalid': submitted && $v.user.password.$error }"                                
                     style="background-image: url(&quot;data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAACIUlEQVQ4EX2TOYhTURSG87IMihDsjGghBhFBmHFDHLWwSqcikk4RRKJgk0KL7C8bMpWpZtIqNkEUl1ZCgs0wOo0SxiLMDApWlgOPrH7/5b2QkYwX7jvn/uc//zl3edZ4PPbNGvF4fC4ajR5VrNvt/mo0Gr1ZPOtfgWw2e9Lv9+chX7cs64CS4Oxg3o9GI7tUKv0Q5o1dAiTfCgQCLwnOkfQOu+oSLyJ2A783HA7vIPLGxX0TgVwud4HKn0nc7Pf7N6vV6oZHkkX8FPG3uMfgXC0Wi2vCg/poUKGGcagQI3k7k8mcp5slcGswGDwpl8tfwGJg3xB6Dvey8vz6oH4C3iXcFYjbwiDeo1KafafkC3NjK7iL5ESFGQEUF7Sg+ifZdDp9GnMF/KGmfBdT2HCwZ7TwtrBPC7rQaav6Iv48rqZwg+F+p8hOMBj0IbxfMdMBrW5pAVGV/ztINByENkU0t5BIJEKRSOQ3Aj+Z57iFs1R5NK3EQS6HQqF1zmQdzpFWq3W42WwOTAf1er1PF2USFlC+qxMvFAr3HcexWX+QX6lUvsKpkTyPSEXJkw6MQ4S38Ljdbi8rmM/nY+CvgNcQqdH6U/xrYK9t244jZv6ByUOSiDdIfgBZ12U6dHEHu9TpdIr8F0OP692CtzaW/a6y3y0Wx5kbFHvGuXzkgf0xhKnPzA4UTyaTB8Ph8AvcHi3fnsrZ7Wore02YViqVOrRXXPhfqP8j6MYlawoAAAAASUVORK5CYII=&quot;); background-repeat: no-repeat; background-attachment: scroll; background-size: 16px 18px; background-position: 98% 50%; cursor: auto;">
                     <div class="input-group-append">
@@ -69,7 +69,7 @@
                     <div class="input-group-prepend">
                       <span class="input-group-text"><i class="fas fa-key"></i></span>
                     </div>                      
-                    <input type="password" class="form-control" placeholder="********"                         
+                    <input type="password" data-cy="confirmPassword"" class="form-control" placeholder="********"                         
                     v-model="user.confirmPassword" id="confirmPassword" name="confirmPassword" 
                     :class="{ 'is-invalid': submitted && $v.user.confirmPassword.$error }" 
                     style="background-image: url(&quot;data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAACIUlEQVQ4EX2TOYhTURSG87IMihDsjGghBhFBmHFDHLWwSqcikk4RRKJgk0KL7C8bMpWpZtIqNkEUl1ZCgs0wOo0SxiLMDApWlgOPrH7/5b2QkYwX7jvn/uc//zl3edZ4PPbNGvF4fC4ajR5VrNvt/mo0Gr1ZPOtfgWw2e9Lv9+chX7cs64CS4Oxg3o9GI7tUKv0Q5o1dAiTfCgQCLwnOkfQOu+oSLyJ2A783HA7vIPLGxX0TgVwud4HKn0nc7Pf7N6vV6oZHkkX8FPG3uMfgXC0Wi2vCg/poUKGGcagQI3k7k8mcp5slcGswGDwpl8tfwGJg3xB6Dvey8vz6oH4C3iXcFYjbwiDeo1KafafkC3NjK7iL5ESFGQEUF7Sg+ifZdDp9GnMF/KGmfBdT2HCwZ7TwtrBPC7rQaav6Iv48rqZwg+F+p8hOMBj0IbxfMdMBrW5pAVGV/ztINByENkU0t5BIJEKRSOQ3Aj+Z57iFs1R5NK3EQS6HQqF1zmQdzpFWq3W42WwOTAf1er1PF2USFlC+qxMvFAr3HcexWX+QX6lUvsKpkTyPSEXJkw6MQ4S38Ljdbi8rmM/nY+CvgNcQqdH6U/xrYK9t244jZv6ByUOSiDdIfgBZ12U6dHEHu9TpdIr8F0OP692CtzaW/a6y3y0Wx5kbFHvGuXzkgf0xhKnPzA4UTyaTB8Ph8AvcHi3fnsrZ7Wore02YViqVOrRXXPhfqP8j6MYlawoAAAAASUVORK5CYII=&quot;); background-repeat: no-repeat; background-attachment: scroll; background-size: 16px 18px; background-position: 98% 50%; cursor: auto;">
@@ -86,7 +86,7 @@
                     <div class="input-group-prepend">
                       <span class="input-group-text"><i class="fas fa-user"></i></span>
                     </div>
-                    <input class="form-control" placeholder="www.DomainName.com" 
+                    <input class="form-control" data-cy="domain" placeholder="www.DomainName.com" 
                     v-model="user.domain" id="domain" name="domain" 
                     :class="{ 'is-invalid': submitted && ($v.user.domain.$error || this.user.domainExists) }">                      
                     <div v-if="submitted && $v.user.domain.$error" class="invalid-feedback">
@@ -103,7 +103,7 @@
                 <div class="my-4">
                   <div class="custom-control custom-checkbox" 
                     v-bind:class="{ 'is-invalid': submitted && $v.user.termsAndConditions.$error }">
-                    <input type="checkbox" class="custom-control-input" id="termsAndConditions" 
+                    <input type="checkbox" data-cy="termsAndConditions" class="custom-control-input" id="termsAndConditions" 
                     v-on:change="$v.user.termsAndConditions.$touch()" v-model="user.termsAndConditions">
                     <label class="custom-control-label" for="termsAndConditions">I agree to the 
                       <a href="#" v-on:click="showTermsAndConditionsModal">terms and conditions</a>
@@ -116,7 +116,7 @@
                 
                   <div class="custom-control custom-checkbox" 
                     v-bind:class="{ 'is-invalid': submitted && $v.user.termsAndConditions.$error }">
-                    <input type="checkbox" class="custom-control-input" id="privacyPolicy"
+                    <input type="checkbox" data-cy="privacyPolicy" class="custom-control-input" id="privacyPolicy"
                     v-on:change="$v.user.privacyPolicy.$touch()" v-model="user.privacyPolicy">
                     <label class="custom-control-label" for="privacyPolicy">I agree to the 
                       <a href="#" v-on:click="showPrivacyPolicyModal">privacy policy</a>
@@ -130,7 +130,7 @@
                 </div>
 
                 <div class="mt-4">
-                  <button type="button" v-bind:disabled="this.disableButton" class="btn btn-block btn-primary" 
+                  <button type="button" id="registeruser" data-cy="registeruser" v-bind:disabled="this.disableButton" class="btn btn-block btn-primary" 
                     v-on:click="RegisterUser()">
                     Create my account
                   </button>
