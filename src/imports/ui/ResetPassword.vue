d5e49200b1f6707cfe08fbd23a0db9d04849836a
Ajit Goel <ajitgoel@gmail.com>
2019-12-22 10:05:11 -0600

    reset password E2E test

--- src/imports/ui/ResetPassword.vue
+++ src/imports/ui/ResetPassword.vue
@@ -25,29 +25,29 @@
                     <div class="input-group-prepend">
                       <span class="input-group-text"><i class="fas fa-user"></i></span>
                     </div>
-                    <input type="email" class="form-control" placeholder="name@example.com" 
+                    <input type="email" data-cy="email" class="form-control" placeholder="name@example.com" 
                     autocomplete="off" v-focus v-model="user.email" id="email" name="email" 
                     :class="{ 'is-invalid': submitted && ($v.user.email.$error || this.user.emailInvalid) }"
                     style="background-image: url(&quot;data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAASCAYAAABSO15qAAAAAXNSR0IArs4c6QAAAPhJREFUOBHlU70KgzAQPlMhEvoQTg6OPoOjT+JWOnRqkUKHgqWP4OQbOPokTk6OTkVULNSLVc62oJmbIdzd95NcuGjX2/3YVI/Ts+t0WLE2ut5xsQ0O+90F6UxFjAI8qNcEGONia08e6MNONYwCS7EQAizLmtGUDEzTBNd1fxsYhjEBnHPQNG3KKTYV34F8ec/zwHEciOMYyrIE3/ehKAqIoggo9inGXKmFXwbyBkmSQJqmUNe15IRhCG3byphitm1/eUzDM4qR0TTNjEixGdAnSi3keS5vSk2UDKqqgizLqB4YzvassiKhGtZ/jDMtLOnHz7TE+yf8BaDZXA509yeBAAAAAElFTkSuQmCC&quot;); background-repeat: no-repeat; background-attachment: scroll; background-size: 16px 18px; background-position: 98% 50%; cursor: auto;">
                     <div v-if="submitted && ($v.user.email.$error || this.user.emailInvalid)" class="invalid-feedback">
                       <span v-if="!$v.user.email.required">Email is required</span>
                       <span v-if="!$v.user.email.email">Email is invalid</span>
-                      <span v-if="this.user.emailInvalid">We could not find your account. Enter a different email or click
+                      <span data-cy="emailinvalid" v-if="this.user.emailInvalid">We could not find your account. Enter a different email or click
                         <router-link v-bind:to="{ name: 'register' }" class="small font-weight-bold">Register</router-link> to continue.
                       </span>
                     </div>
                   </div>
                 </div>
                 <div class="mt-4">
-                  <button type="button" class="btn btn-block btn-primary" v-bind:disabled="this.disableButton" v-on:click="ResetPassword()">
+                  <button type="button" data-cy="resetpassword" class="btn btn-block btn-primary" v-bind:disabled="this.disableButton" v-on:click="ResetPassword()">
                     Reset password
                   </button>
                 </div>
               </form>              
 
               <br/>
-              <SuccessAlert ref="successAlert" v-bind:message='this.successMessage'/>
-              <ErrorAlert ref="errorAlert" v-bind:message='this.failureMessage'/>
+              <SuccessAlert ref="successAlert" data-cy="successalert" v-bind:message='this.successMessage'/>
+              <ErrorAlert ref="errorAlert" data-cy="erroralert" v-bind:message='this.failureMessage'/>
 
               <div class="mt-4 text-center"><small>Not registered?</small>
                 <router-link v-bind:to="{ name:'register'}" class="small font-weight-bold">Create account</router-link>
